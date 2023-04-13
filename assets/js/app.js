56f75da10d96631f3c247c21c1ee387b75c48c09
CarlMurray <carl.j.murray@gmail.com>
2023-04-13 20:06:25 +0000

    add function to fetch attraction data, API KEY not added for dev

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
+const API_URL = 'https://failteireland.azure-api.net/opendata-api/v1/attractions';
+
 // Initialize and add the map, code copied from Maps API documentation
 let map;
+let data;
 
 async function initMap() {
   // The location of Uluru
@@ -26,5 +29,25 @@ const addMarker = async () => {
     });
   }
 
+// function to fetch attraction data from Failte Ireland API
+async function fetchData() {
+    const getData = await fetch(API_URL)
+    data = await getData.json()
+    console.log(data);
+    return data;
+}
+
 initMap();
 addMarker();
+fetchData();
+
+// backup for reference
+// function to add marker to map, code copied from Maps API documentation and modified
+// const addMarker = async () => {
+//     // The marker, positioned at Ireland
+//     const { Marker } = await google.maps.importLibrary("marker");
+//     const marker = new Marker({
+//       map: map,
+//       position: { lat: 53.4152431, lng: -7.9559668 },
+//     });
+//   }
