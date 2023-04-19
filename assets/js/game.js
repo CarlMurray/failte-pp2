e203690d4ea604531b28b1f3e25647a6369d9368
CarlMurray <carl.j.murray@gmail.com>
2023-04-19 20:58:21 +0100

    decouple map and street view so independant

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,41 @@
-function initialize() {
-    const fenway = { lat: 42.345573, lng: -71.098326 };
-    const map = new google.maps.Map(document.getElementById("game-map-container"), {
-      center: fenway,
-      zoom: 14,
+const CSV_PATH = 'assets/data/attractions.json'
+
+async function initMap() {
+    // SET INIT POSITION AT IRELAND
+    const position = { lat: 53.4152431, lng: -7.9559668 };
+    const { Map } = await google.maps.importLibrary("maps");
+    map = new Map(document.getElementById("game-map-container"), {
+        mapId: "47f8f1437cc57452", // PERSONAL GMAPS ID WITH CUSTOM STYLES
+        zoom: 7,
+        center: position,
     });
-    const panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("game-street-container"),
-      {
-        position: fenway,
-        pov: {
-          heading: 34,
-          pitch: 10,
-        },
-      }
-    );
-  
-    map.setStreetView(panorama);
-  }
-  
-  window.initialize = initialize;
+
+}
+
+// FETCH ATTRACTION DATA FROM FAILTE IRELAND CSV attractions.json
+async function fetchData() {
+    const getData = await fetch(CSV_PATH);
+    data = await getData.json();
+    console.log(data);
+    return data;
+}
+
+function initialize() {
+    const position = { lat: 53.4152431, lng: -7.9559668 };
+    const map = new google.maps.Map(document.getElementById("game-map-container"), {
+        center: position,
+        zoom: 7,
+        streetViewControl: false,
+        mapId: "47f8f1437cc57452", // PERSONAL GMAPS ID WITH CUSTOM STYLES
+    });
+    panorama = new google.maps.StreetViewPanorama(
+        document.getElementById("game-street-container"),
+        {
+          position: { lat: 37.86926, lng: -122.254811 },
+          pov: { heading: 165, pitch: 0 },
+          zoom: 1,
+        }
+      );
+}
+
+window.initialize = initialize;
