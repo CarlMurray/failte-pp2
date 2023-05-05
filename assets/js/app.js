38e1d6d0aab4ab1df685d8cc06f70e568a107bd8
CarlMurray <carl.j.murray@gmail.com>
2023-05-05 21:01:04 +0100

    move gmaps API loader from index.html to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,44 @@
+// ADD GOOGLE MAPS JAVASCRIPT API - FROM GOOGLE MAPS JAVASCRIPT API DOCUMENTATION
+((g) => {
+  var h,
+    a,
+    k,
+    p = "The Google Maps JavaScript API",
+    c = "google",
+    l = "importLibrary",
+    q = "__ib__",
+    m = document,
+    b = window;
+  b = b[c] || (b[c] = {});
+  var d = b.maps || (b.maps = {}),
+    r = new Set(),
+    e = new URLSearchParams(),
+    u = () =>
+      h ||
+      (h = new Promise(async (f, n) => {
+        await (a = m.createElement("script"));
+        e.set("libraries", [...r] + "");
+        for (k in g)
+          e.set(
+            k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+            g[k]
+          );
+        e.set("callback", c + ".maps." + q);
+        a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+        d[q] = f;
+        a.onerror = () => (h = n(Error(p + " could not load.")));
+        a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+        m.head.append(a);
+      }));
+  d[l]
+    ? console.warn(p + " only loads once. Ignoring:", g)
+    : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+})({
+  key: "AIzaSyCbslAH8fUvbb6O544Xyq0iJBzK50vvX08",
+  // Add other bootstrap parameters as needed, using camel case.
+  // Use the 'v' parameter to indicate the version to load (alpha, beta, weekly, etc.)
+});
+
 const JSON_PATH = "assets/data/attractions.json"; //FILE PATH FOR ATTRACTION DATA
 let map; //GMAPS OBJECT
 let data; //FOR FETCHED LOCATION DATA
@@ -75,7 +116,7 @@ const initMarkers = async () => {
   // ITERATE THROUGH ALL DATA POINTS, DEFINE VARIABLES,
   // DEFINE MARKER ICONS, DEFINE CONTENT
   for (let i = 0; i < data.length; i++) {
-    
+
     // DETRUCTURE OBJ TO DEFINE VARIABLES
     const {
       Latitude: lat,
